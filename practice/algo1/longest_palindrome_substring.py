f08411d6041a1ed0d2ec7bd9c7e048d58856639a
Sang-Hyeong Jin <jwjln3126@naver.com>
2021-12-29 09:18:43 +0900

    Create longest_palindrome_substring.py

---
+++ practice/algo1/longest_palindrome_substring.py
@@ -0,0 +1,23 @@
+# 가장 긴 팰린드롬 부분 문자열 출력
+
+
+def palin(s: str) -> str:
+    def expand(left: int, right: int) -> str:
+        while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+            left -= 1
+            right += 1
+
+        return s[left + 1:right - 1]
+
+    if len(s) < 2 or s == s[::-1]:
+        return s
+
+    result = ''
+
+    for i in range(len(s)-1):
+        result = max(result, expand(i,i+1), expand(i,i+2), key=len)
+
+    return result
+
+s = 'babad'
+print(palin(s))
