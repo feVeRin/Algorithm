2f9271b54f3dadda0f50bf1d937c9d13913d41e4
Sang-Hyeong Jin <jwjln3126@naver.com>
2021-12-30 08:06:23 +0900

    Create q1.py

---
+++ practice/algo2/shortestpath/q1.py
@@ -0,0 +1,29 @@
+INF = int(1e9)
+
+n, m = map(int, input().split())
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        for k in range(1, n + 1):
+            graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k])
+
+res = 0
+for i in range(1, n+1):
+    count = 0
+    for j in range(1, n+1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    if count == n:
+        res += 1
+
+print(res)
