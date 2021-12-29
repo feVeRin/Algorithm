03fa6879a8c22527c789a88e54d46d1d6abcac2d
Sang-Hyeong Jin <jwjln3126@naver.com>
2021-12-29 09:19:15 +0900

    Create 10101.py

---
+++ problems/10101.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/10101
+
+import sys
+
+input = sys.stdin.readline
+
+angle = [int(input()) for i in range(3)]
+
+if sum(angle) == 180:
+    if angle[0] == angle[1] == angle[2]:
+        print('Equilateral')
+    elif (angle[0] == angle[1]) or (angle[0] == angle[2]) or (angle[1] == angle[2]):
+        print('Isosceles')
+    else:
+        print('Scalene')
+else:
+    print('Error')
