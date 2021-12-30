33ab592c802ea27d38a2bb65b9276e2beb11ae75
Sang-Hyeong Jin <jwjln3126@naver.com>
2021-12-31 08:54:38 +0900

    Create 10870.py

---
+++ problems/10870.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/10870
+
+import collections
+import sys
+
+
+tb = collections.defaultdict(int)
+
+
+def fibo(N):
+    tb[1] = 1
+    for i in range(2, N+1):
+        tb[i] = tb[i-1] + tb[i-2]
+
+    return tb[N]
+
+
+n = int(sys.stdin.readline())
+print(fibo(n))
