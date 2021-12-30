e1182fdec84cee3ab2c4fbdd83b1d5335aac2484
Sang-Hyeong Jin <jwjln3126@naver.com>
2021-12-31 08:54:51 +0900

    Create 11051.py

---
+++ problems/11051.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/11051
+
+import sys
+from math import factorial
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+    print((factorial(n) // (factorial(m) * factorial(n-m))) % 10007)
