edc5c5bdb2a3c8ebd6ecc3655e70361761726d18
Sang-Hyeong Jin <jwjln3126@naver.com>
2022-01-03 08:46:30 +0900

    Create 18310.py

---
+++ problems/18310.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/18310
+
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+house = list(map(int, input().split()))
+house.sort()
+print(house[(n-1)//2])
