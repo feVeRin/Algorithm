e14a0715f7dd01fe1a244a077c300dbc5bb1fed6
Sang-Hyeong Jin <jwjln3126@naver.com>
2023-06-09 08:21:37 +0900

    Create 10811.py

---
+++ problems/10811.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+basket = list(range(1, N+1))
+for _ in range(M):
+    i, j = map(int, input().split())
+    basket[i-1:j] = basket[i-1:j][::-1]
+    
+for b in basket:
+    print(b, end=' ')
