764882d9937caae233e7d8cfb3e763710772f7d6
Sang-Hyeong Jin <jwjln3126@naver.com>
2024-07-24 19:39:54 +0900

    Create 10250.py

---
+++ problems/10250.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    h, w, n = map(int, input().split())
+    
+    floor = n % h 
+    room = (n // h) + 1
+    if floor == 0:
+        floor = h
+        room -= 1
+    
+    print(floor * 100 + room)
