6cd84157211b22ca9ddc3bce1db86097294485fc
Sang-Hyeong Jin <jwjln3126@naver.com>
2024-07-24 19:40:22 +0900

    Create 4104.py

---
+++ problems/4104.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    x, y = map(int, input().split())
+    if x ==0 and y==0:
+        break
+    
+    if x>y:
+        print('Yes')
+    else:
+        print('No')
